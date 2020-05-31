b06a2bf74af6f92718a7a4793eaf9bc0a2c5e3ac
David Gonzalez <daviddagb2@gmail.com>
2020-05-31 13:48:57 -0600

    Agregados modelos

---
+++ app/PostType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PostType extends Model
+{
+    protected $fillable = [
+        'name',
+        'icon',
+        'active',
+    ];
+}
